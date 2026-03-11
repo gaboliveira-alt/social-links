53c6644ae56611873a05e473b8fbce07aa83c7b9
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-03-11 17:51:08 -0300

    Add Button.tsx

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import type React from "react";
+import { tv } from "tailwind-variants";
+
+const variants = tv({
+    base: "bg-yellow w-full px-4 py-2 outline-none cursor-pointer rounded-3xl text-white not-disabled:hover:bg-yellow/80 font-medium duration-500 not-disabled:scale-95 disabled:opacity-45 disabled:cursor-not-allowed",
+    variants: {
+        variant: {
+            small: "w-auto py-1 px-5 text-sm"
+        },
+    },
+});
+
+type Props = {
+    children: React.ReactNode;
+    variant?: keyof (typeof variants)["variants"]["variant"]; 
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = ({ children, variant, ...props }: Props) => {
+    return (
+        <button className={variants({ variant })} {...props}>
+            {children}
+        </button>
+    )
+}
